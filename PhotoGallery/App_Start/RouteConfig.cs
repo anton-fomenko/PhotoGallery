4b66d9e3abb0693544c8b09a68f611d55f8d637a
ot-anton-fomenko <anton.fomenko@dataart.com>
2016-10-20 18:44:14 +0300

    Delete unnecessary views.

--- PhotoGallery/App_Start/RouteConfig.cs
+++ PhotoGallery/App_Start/RouteConfig.cs
@@ -18,7 +18,7 @@ namespace PhotoGallery
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Albums", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
