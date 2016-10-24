8d7d60b3cf83e98093d96ba8bb2b7e0d04b26562
ot-anton-fomenko <anton.fomenko@dataart.com>
2016-10-24 17:47:53 +0300

    Add new attribute for validdation of file type on the server side.

---
+++ PhotoGallery/Attributes/MaxFileSizeAttribute.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoGallery.Attributes
+{
+    public class MaxFileSizeAttribute : ValidationAttribute, IClientValidatable
+    {
+        private readonly int _maxFileSize;
+        public MaxFileSizeAttribute(int maxFileSize)
+        {
+            _maxFileSize = maxFileSize;
+        }
+
+        public override bool IsValid(object value)
+        {
+            var file = value as HttpPostedFileBase;
+            if (file == null)
+            {
+                return false;
+            }
+
+            bool isFileSizeValid = file.ContentLength <= _maxFileSize;
+
+            bool isMimeTypeValid = false;
+
+            if (file.ContentType.Contains("image"))
+            {
+                string[] formats = new string[] { ".jpg", ".jpeg" };
+                isMimeTypeValid = formats.Any(item => file.FileName.EndsWith(item, StringComparison.OrdinalIgnoreCase));
+            }
+
+            return isFileSizeValid && isMimeTypeValid;
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return base.FormatErrorMessage(_maxFileSize.ToString());
+        }
+
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        {
+            var rule = new ModelClientValidationRule
+            {
+                ErrorMessage = FormatErrorMessage(_maxFileSize.ToString()),
+                ValidationType = "filesize"
+            };
+            rule.ValidationParameters["maxsize"] = _maxFileSize;
+            yield return rule;
+        }
+    }
+}
--- PhotoGallery/Models/CreatePhotoViewModel.cs
+++ PhotoGallery/Models/CreatePhotoViewModel.cs
@@ -1,10 +1,5 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
-using PhotoGallery.Domain;
+﻿using System.Web;
+using PhotoGallery.Attributes;
 
 namespace PhotoGallery.Models
 {
@@ -12,6 +7,9 @@ namespace PhotoGallery.Models
     {
         public string Name { get; set; }
         public string Description { get; set; }
+
+        [MaxFileSize(500 * 1024, 
+            ErrorMessage = "Maximum allowed file size is 500 kilobytes")]
         public HttpPostedFileBase File { get; set; }
     }
 }
