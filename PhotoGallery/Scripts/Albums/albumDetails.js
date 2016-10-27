b4ea5b98df2e251eba4b76b55691d452357d1c29
ot-anton-fomenko <anton.fomenko@dataart.com>
2016-10-27 20:11:54 +0300

    Add delete button to album details view.

---
+++ PhotoGallery/Scripts/Albums/albumDetails.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    $(".deletePhotos")
+        .on('click',
+            function () {
+                var checkboxes = $(".img-checkbox");
+                var arrayOfPhotoIds = [];
+
+                checkboxes.each(function () {
+                    arrayOfPhotoIds.push($(this).data("photoid"));
+                });
+
+                $.ajax({
+                    type: "DELETE",
+                    url: '/api/album',
+                    data: JSON.stringify(arrayOfPhotoIds),
+                    success: function (data) {
+                        location.reload();
+                    }
+                });
+            });
+});
