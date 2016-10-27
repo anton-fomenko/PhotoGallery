40001b1bcd929c1299a2ca60bab8b117d7877436
ot-anton-fomenko <anton.fomenko@dataart.com>
2016-10-27 18:07:37 +0300

    Added image custom helper

---
+++ PhotoGallery/Helpers/HtmlHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoGallery.Helpers
+{
+    public static class HtmlHelpers
+    {
+        public static IHtmlString Image(this HtmlHelper helper, string src, string alt)
+        {
+            TagBuilder tb = new TagBuilder("img");
+            tb.Attributes.Add("src", VirtualPathUtility.ToAbsolute(src));
+            tb.Attributes.Add("alt", alt);
+            return new MvcHtmlString(tb.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
