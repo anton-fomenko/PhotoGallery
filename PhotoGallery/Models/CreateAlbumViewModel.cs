89e599b88657827970b6952a1168d14a28850994
ot-anton-fomenko <anton.fomenko@dataart.com>
2016-10-31 11:34:31 +0200

    Prohibit creation of the album without main photo selected.

--- PhotoGallery/Models/CreateAlbumViewModel.cs
+++ PhotoGallery/Models/CreateAlbumViewModel.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using PhotoGallery.Domain;
+using System.ComponentModel.DataAnnotations;
 
 namespace PhotoGallery.Models
 {
     public class CreateAlbumViewModel
     {
         public string Name { get; set; }
+
         public string Description { get; set; }
+
         public List<Photo> PhotosOfTheUser { get; set; }
+
+        [Required]
+        [Display(Name="Main Photo")]
         public int SelectedMainPhotoId { get; set; }
     }
 }
